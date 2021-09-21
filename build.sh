72cb691a646a5bb4b751d5f10870869366508fa8
Gogs <gogs@fake.local>
2021-09-21 09:14:23 +0200

    updated build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./node_modules/.bin/pkg -t node14-linux-x64 -o lightdb-server --compress GZip server.js
+./node_modules/.bin/pkg -t node14-linux-x64 -o dist/lightdb-server --compress GZip server.js
+cp -R public dist/public
